2ff986af0bb4d3c81cf7dfa523be50335d23c3fe
j2gg0s <j2gg0s@gmail.com>
2023-02-20 10:24:17 +0800

    rename

---
+++ cloud-ubuntu-init.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+systemctl is-active docker
+if [[ $? -ne 0 ]]; then
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  apt update
+  apt install -y docker-ce
+fi
